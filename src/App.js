e85e99ad585609157f0f196474750b770019aa9f
lwittman <louise.wittmann@ecl20.ec-lyon.fr>
2022-03-16 21:29:33 +0100

    fin seance

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import loading from './loading.svg';
 import './App.css';
 import Sound from 'react-sound';
 import Button from './Button';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-const apiToken = '<<Copiez le token de Spotify ici>>';
+
+const apiToken = 'BQDZD8fYQhT9meGX4OKEcvjSaWJC8v3yX9VkUXq3AmfvcY42uXp2yyxWndSGY2FOUhakF2GkY-WhNvdcCE-cp-pvX7zhjKtJcfUqrAZUHxRbkHgS9c-7Q8Dohd8XXlKyxEX57sxyzS2jvKzW3k-T5Ifn8vdTGUmQnA';
 
 function shuffleArray(array) {
   let counter = array.length;
@@ -29,18 +32,75 @@ function getRandomNumber(x) {
 }
 
 const App = () => {
-  return (
+
+  const [songsLoaded, setSongsLoaded] = useState(false);
+  const [text, setText] = useState('');
+  const[tracks, setTracks] = useState([]);
+  const i = getRandomNumber(20);
+
+  const button0 = tracks[0];
+  const button1 = tracks[1];
+  const button2 = tracks[2];
+
+
+
+  useEffect(() => {fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+     Authorization: 'Bearer ' + apiToken,
+    },
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log("Réponse reçue ! Voilà ce que j'ai reçu : ", data);
+      setTracks(data["items"]);
+      setSongsLoaded(true);
+
+    })}, []);
+
+  if (songsLoaded){
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
         <h1 className="App-title">Bienvenue sur le Blindtest</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai Blindtest ! YAY c'est quoi cette sorcellerie</p>
+        <p>{tracks[0].track.name}</p>
+        <AlbumCover track = {tracks[0].track} />
+        <Sound url={tracks[0].track.preview_url} playStatus={Sound.status.PLAYING} muted={true}/>
+
       </div>
       <div className="App-buttons">
+      <Button onClick={doSomething}>{button0.track.name}</Button>
+      <Button onClick={doSomething}>{button1.track.name}</Button>
+      <Button onClick={doSomething}>{button2.track.name}</Button>
       </div>
     </div>
+    );
+  }
+  else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <h1 className="App-title">Bienvenue sur le Blindtest</h1>
+        </header>
+        <div className="App-images">
+          <img src={loading} className="App-logo" alt="logo"/>
+        </div>
+        <div className="App-buttons">
+        </div>
+      </div>
+      );
+  }
+}
+
+const AlbumCover = (props) =>  {
+  const t = props.track;
+  const src = t.album.images[0].url;
+  return (
+      <img src={src} style={{ width: 400, height: 400 }} />
   );
 }
 
